b5c69cec9639bd5cf4187beda654e9c4f0d63e74
Colgamma <gtrmr87@gmail.com>
2019-12-11 20:30:40 +0300

    +

---
+++ src/server.py
@@ -0,0 +1,69 @@
+#  Created by Artem Manchenkov
+#  artyom@manchenkoff.me
+#
+#  Copyright © 2019
+#
+#  Сервер для обработки сообщений от клиентов
+#
+from twisted.internet import reactor
+from twisted.internet.protocol import ServerFactory, connectionDone
+from twisted.protocols.basic import LineOnlyReceiver
+
+
+class ServerProtocol(LineOnlyReceiver):
+    factory: "Server"
+    login: str = None
+
+    def connectionMade(self):
+        self.factory.clients.append(self)
+
+    def connectionLost(self, reason=connectionDone):
+        self.factory.clients.remove(self)
+
+    def lineReceived(self, line: bytes):
+        content = line.decode(errors='ignore')
+
+        if self.login is not None:
+            content = f"Masseg from {self.login}:{content}"
+
+            for user in self.factory.clients:
+                if user is not self:
+                    user.sendLine(content.encode())
+        else:
+            # login:admin -> admin
+            if content.startswith("login:"):
+                login = content.replace("login:", "")
+                unique = True
+
+                for user in self.factory.clients:
+                    if login == user.login:
+                        unique = False
+
+                if unique:
+                    self.login = login
+                    self.sendLine("Welcome!".encode())
+
+                    for message in self.factory.messages:
+                        self.sendLine(message.encode())
+                else:
+                    self.sendLine(f"Клиент {login} занят, попробуйте другой".encode())
+                    self.transport.loseConnection()
+            else:
+                self.sendLine("invalid login".encode())
+
+
+class Server(ServerFactory):
+    protocol = ServerProtocol
+    clients: list
+    messages: list = []
+
+    def startFactory(self):
+        self.clients = []
+        print("Server started")
+
+    def stopFactory(self):
+        print("Server closed")
+
+
+reactor.listenTCP(1234, Server())
+reactor.run()
